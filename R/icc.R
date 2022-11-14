e3d2c36f970f85a340d0cb152b524aba3c8659db
jmgirard <me@jmgirard.com>
2022-11-13 23:17:47 -0600

    Update icc functions

--- R/icc.R
+++ R/icc.R
@@ -6,26 +6,43 @@ icc_a_k <- function(vs, vr, vsr, k) {
   vs / (vs + (vr + vsr) / k)
 }
 
-icc_c_1 <- function(vs, vr, vsr) {
+icc_c_1 <- function(vs, vsr) {
   vs / (vs + vsr)
 }
 
-icc_c_k <- function(vs, vr, vsr, k) {
+icc_c_k <- function(vs, vsr, k) {
   vs / (vs + vsr / k)
 }
 
-icc_1 <- function(vs, vrins) {
-  vs / (vs + vrins)
+icc_a_khat <- function(vs, vr, vsr, khat) {
+  vs / (vs + (vr + vsr) / khat)
 }
 
-icc_k <- function(vs, vrins, k) {
-  vs / (vs + vrins / k)
+icc_q_khat <- function(vs, vr, vsr, q, khat) {
+  vs / (vs + (q * vr) + (vsr / khat))
 }
 
-khat <- function(k) {
-  (sum(k^-1) / length(k))^-1
+khat <- function(ks) {
+  (sum(ks^-1) / length(ks))^-1
 }
 
-# function to calculate q
+# srm <- matrix(sample(0:1, size = 30, replace = TRUE), nrow = 10)
 
-# k
+qkhat <- function(srm) {
+  n <- nrow(srm)
+  ks <- rowSums(srm)
+  spairs <- combn(n, 2)
+
+  numer <- function(spair, srm) {
+    s <- spair[[1]]
+    sp <- spair[[2]]
+    ks <- rowSums(srm)
+    ks_s <- ks[[s]]
+    ks_sp <- ks[[sp]]
+    ks_ssp <- sum(colSums(srm[c(s, sp), ]) > 1)
+    ks_ssp / (ks_s * ks_sp)
+  }
+
+  numerator <- sum(apply(X = spairs, MARGIN = 2, FUN = numer, srm))
+  (1 / khat(ks)) - (numerator / (n * (n - 1)))
+}
